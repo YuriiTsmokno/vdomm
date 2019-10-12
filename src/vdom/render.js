619039d6dea3a1916df8d89d1693aa2b29912fc0
yurii_89 <suro1989@ukr.net>
2019-10-12 15:17:08 +0300

    renderElement

---
+++ src/vdom/render.js
@@ -0,0 +1,21 @@
+const renderElement = ({tagName, attrs, children}) => {
+    const $el = document.createElement(tagName);
+
+    for(const [key, value] of Object.entries(attrs)) {
+        $el.setAttribute(key, value);
+    }
+
+    for(const child of children) {
+        $el.appendChild(render(child));
+    }
+}
+
+const render = (vNode) => {
+   if(typeof vNode === 'string') {
+       return document.createTextNode(vNode);
+   }
+
+   return renderElement(vNode);
+}
+
+export default render;
